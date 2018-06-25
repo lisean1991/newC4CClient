ef6f8f4f1bfd2c20377ef654b66beb98637f9c29
Sean Li <sean.li01@sap.com>
2018-06-25 14:34:23 +0800

    V5

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,6 @@ const API_C4C = '/api/v2';
 const APPOINTMENT = () => `${API_V2}/appointment`;
 
 export default {
-  APPOINTMENT: 'http://testc4cwc.duapp.com/mini/appointment',
+  APPOINTMENT_OTHER: 'http://testc4cwc.duapp.com/mini/appointment',
+  APPOINTMENT
 };
